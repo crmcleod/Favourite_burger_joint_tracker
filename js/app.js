4ecaea99a30fe20046ed99a5ea1ab9cfdb0d1b94
Craig McLeod <mcleod.craig@outlook.com>
2020-06-19 14:01:32 +0100

    Full form saving working

--- js/app.js
+++ js/app.js
@@ -7,8 +7,31 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleSaveButtonSubmit = function (event) {
     event.preventDefault();
 
-    const restaurantListItem = createRestaurantListItem(event.targer);
+    const restaurantListItem = createRestaurantListItem(event.target);
     const restaurantList = document.querySelector('#restaurant-list');
     restaurantList.appendChild(restaurantListItem);
     event.target.reset();
+}
+
+const createRestaurantListItem = function (form) {
+    const restaurantListItem = document.createElement('li');
+    restaurantListItem.classList.add('restaurant-list-item');
+
+    const restaurant = document.createElement('h3');
+    restaurant.textContent = form.restaurant.value;
+    restaurantListItem.appendChild(restaurant);
+
+    const country = document.createElement('h4');
+    country.textContent = form.country.value;
+    restaurantListItem.appendChild(country);
+
+    const city = document.createElement('h4');
+    city.textContent = form.city.value;
+    restaurantListItem.appendChild(city);
+
+    const speciality = document.createElement('h4');
+    speciality.textContent = form.speciality.value;
+    restaurantListItem.appendChild(speciality);
+
+    return restaurantListItem;
 }
